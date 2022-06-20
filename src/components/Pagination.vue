8a164e383d12debe07c87cdb865d171b57680bbd
AlexandrGashevsky <71451324+AlexandrGashevsky@users.noreply.github.com>
2022-06-20 18:06:50 +0300

    c

---
+++ src/components/Pagination.vue
@@ -0,0 +1,51 @@
+<script setup></script>
+
+
+<template>
+<div class="pagination-section">
+    <ul class="pagination">
+        <li>←</li>
+        <li v-for="(index) in countPages(n)">{{index}}</li>
+        <li>→</li>
+    </ul>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'pagination',
+    props:{
+        n: Number
+    },
+    methods: {
+        countPages(arrayLength){
+            if(arrayLength <= 3){
+                return 1;
+            } else {
+              return Math.ceil(arrayLength/3);
+            }
+        }
+    }
+}
+</script>
+
+<style>
+    .pagination{
+        padding: 0;
+        margin-left: auto;
+        margin-right: auto;
+        list-style: none;
+        display: flex;
+        width: 400px;
+        align-items: center;
+    }
+    .pagination > * {
+        border: solid 1px rgb(65, 65, 65);
+        border-radius: 3%;
+        background-color: rgb(231, 239, 245);
+        text-align: center;
+        font-size: 30px;
+        margin: auto;
+        width: 36px
+    }
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
+import { resolveComponents } from 'uri-js';
 import { createStore } from 'vuex'
-import json from '../data/events.json'
+import axios from 'axios'
+
 
 export default createStore({
   state: {
-    events: json.events
+    events: []
   },
   getters: {
     allEvents(state){
@@ -16,14 +18,42 @@ export default createStore({
     },
     deleteEvent({commit}, index){
       commit('DELETE_EVENT', index)
+    },
+    setEvents({commit}, events){
+      commit('SET_EVENTS', events)
+    },
+    editEvent({commit}, event, index){
+      commit('EDIT_EVENT', event, index)
     }
   },
   mutations: {
     ADD_EVENT(state, event) {
-      state.events.push(event);
+     // state.events.push(event);
+     axios.post('http://localhost:3000/events', event).then(
+      state.events.push(event)
+     );
+      
     },
     DELETE_EVENT(state, index){
+      
+      axios.delete(`http://localhost:3000/events/${state.events[index].id}`).then(
       state.events.splice(index, 1)
+     )
+    },
+    SET_EVENTS(state, events){
+      state.events = events
+    },
+    EDIT_EVENT(state, event){
+      axios.put(`http://localhost:3000/events/${event.id}`, event).catch(
+        (error) => {
+          alert("Try another ID \n" + error);
+        }
+      ).then(
+        ()=>{
+          const id = state.events.findIndex(x => x.id === event.id);
+          state.events[id] = event;
+        }
+      );
     }
   }
 })
